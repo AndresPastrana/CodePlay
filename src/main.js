883a9290c87412eaf0b0b91e431b63f9d5d1a20b
AndresPastrana <andreserluis@gmail.com>
2024-12-29 16:57:30 -0300

    fix: load template when error decoding the url values

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import { welcomeStringHtml } from "./template/welcome";
-import { welcomeStyles } from "./template/styles";
+import { welcomeStringHtml, welcomeStyles, welcomJs } from "./template/welcome.template";
 import { cssEditor, htmlEditor, jsEditor } from "./editors";
 import { updatePreview } from "./features";
 
@@ -7,11 +6,23 @@ function init () {
   // Ge the current values of each editor
   const [html, css, js] = window.location.pathname.split("%7C");
 
+  try {
+    const decodedHtml = window.atob(html.slice(1))
+    const decodedCss = window.atob(css)
+    const decodedJs = window.atob(js)
+
+    htmlEditor.setValue(decodedHtml);
+    cssEditor.setValue(decodedCss);
+    jsEditor.setValue();
+
+    updatePreview(decodedJs);
+  } catch (error) {
+    console.log("If it was an error decoding the url values load the default template");
+    htmlEditor.setValue(welcomeStringHtml);
+    cssEditor.setValue(welcomeStyles);
+    jsEditor.setValue(welcomJs);
+  }
   // Set the values for each editors
-  htmlEditor.setValue(window.atob(html.slice(1)) || welcomeStringHtml);
-  cssEditor.setValue(window.atob(css) || welcomeStyles);
-  jsEditor.setValue(window.atob(js));
-  updatePreview();
 }
 
 htmlEditor.onDidChangeModelContent(updatePreview);
--- src/template/welcome.template.js
+++ src/template/welcome.template.js
@@ -101,3 +101,21 @@ body.light-theme .theme-btn {
     color: #333;
 }
 `;
+
+export const welcomeStringHtml = ` <div class="editor-wrapper">
+<header class="editor-header">
+    <h1> Live Code Editor</h1>
+    <div class="theme-toggle">
+        <button id="toggleTheme" class="theme-btn">Toogle</button>
+    </div>
+</header>
+<div class="editor-container">
+    <div class="editor-area">
+        <textarea id="editor" class="editor" placeholder="Write your code here..."></textarea>
+    </div>
+    <div class="preview-area">
+        <iframe id="preview" class="preview" title="Live Preview"></iframe>
+    </div>
+</div>`;
+
+export const welcomJs = ``
